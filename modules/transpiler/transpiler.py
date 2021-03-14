ad8a6854f119c2cd9edcabe6fc4bc3cb6fa74eb9
David <davidjmacdonald11@gmail.com>
2021-03-14 15:02:07 -0400

    Clean up relative import

--- modules/transpiler/transpiler.py
+++ modules/transpiler/transpiler.py
@@ -1,4 +1,4 @@
-from ..transpiler.scope_manager import ScopeManager
+from .scope_manager import ScopeManager
 
 def transpile(filename, new_filename):
     with open(filename) as seafile, open(new_filename, "w") as cfile:
