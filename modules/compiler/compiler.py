d0374dd4dcf406afd0abf27abe4a802032bcf7c3
David <davidjmacdonald11@gmail.com>
2021-04-16 17:54:58 -0400

    Remove unecessary methods from Compiler

--- modules/compiler/compiler.py
+++ modules/compiler/compiler.py
@@ -2,12 +2,3 @@ from modules.visitor.visitor import Visitor
 
 class Compiler(Visitor):
     vocab_base = "Compil"
-
-    def visit_number_node(self, node):
-        pass
-
-    def visit_binary_operation_node(self, node):
-        pass
-
-    def visit_unary_operation_node(self, node):
-        pass
