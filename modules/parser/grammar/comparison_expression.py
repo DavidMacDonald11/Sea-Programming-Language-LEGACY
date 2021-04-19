9065a5970527799ce0f712eb7b0758f1a36d8e1b
David <davidjmacdonald11@gmail.com>
2021-04-19 11:08:23 -0400

    Make comparison expressions behave like in Python ex:(0 < 4 < 5)

--- modules/parser/grammar/comparison_expression.py
+++ modules/parser/grammar/comparison_expression.py
@@ -1,4 +1,6 @@
+from modules.lexer.position import Position
 from modules.lexer.token_types import TT
+from modules.lexer.token import Token
 from .bitwise.or_expression import make_bitwise_or_expression
 from ..nodes.collection import NODES
 
@@ -12,6 +14,23 @@ def make_comparison_expression(parser, **make_funcs):
         return NODES.LeftUnaryOperationNode(operation_token, node)
 
     operations = (TT.EQ, TT.NE, TT.LT, TT.GT, TT.LTE, TT.GTE)
-    args = (parser, make_bitwise_or_expression, operations)
 
-    return make_funcs["binary_operation"](*args, **make_funcs)
+    left = make_bitwise_or_expression(parser, **make_funcs)
+    right = None
+
+    while parser.token.type in operations:
+        operation = parser.take_token()
+        new_right = make_bitwise_or_expression(parser, **make_funcs)
+
+        if right is None:
+            left = NODES.BinaryOperationNode(left, operation, new_right)
+        else:
+            position = Position(right.position.end, operation.position.start)
+            and_token = Token(TT.KEYWORD, "and", position)
+
+            right = NODES.BinaryOperationNode(right, operation, new_right)
+            left = NODES.BinaryOperationNode(left, and_token, right)
+
+        right = new_right
+
+    return left
