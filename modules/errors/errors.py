f1d45c2faf39927e41e24ed425afae8a7fe25424
David <davidjmacdonald11@gmail.com>
2021-04-26 11:51:33 -0400

    Add position to error and add construct_all() to token

--- modules/errors/errors.py
+++ modules/errors/errors.py
@@ -1,6 +1,8 @@
+from position.position import Position
+
 class SeaError(Exception):
     def __init__(self, position = None, message = ""):
-        self.position = position
+        self.position = position if position is not None else Position(None)
         self.message = message
         super().__init__(message)
 
