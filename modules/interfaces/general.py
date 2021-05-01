65785d75211d11bfa53e2c6c73f6a0f5d7d3f770
David MacDonald <davidjmacdonald11@gmail.com>
2021-05-01 10:56:03 -0400

    Hook up parser to general interface

--- modules/interfaces/general.py
+++ modules/interfaces/general.py
@@ -1,11 +1,13 @@
-from preprocessor.preprocessor import PreProcessor
+from parser.parser import Parser
+from parser.errors import ParserError
 from lexer.lexer import Lexer
 from lexer.errors import LexerError
+from preprocessor.preprocessor import PreProcessor
 from errors.errors import SeaError
 
 def main(streams, debug):
     try:
-        preprocessor = lexer = None
+        preprocessor = lexer = parser = None
         in_file = None
 
         preprocessor = PreProcessor(streams.in_stream)
@@ -13,13 +15,17 @@ def main(streams, debug):
 
         LexerError.lexer = lexer = Lexer(in_file)
         lexer.make_tokens()
+
+        ParserError.parser = parser = Parser(lexer)
+        parser.make_ast()
     except SeaError as error:
         streams.error_stream.write(error)
     finally:
-        print_debug_info(debug, streams.debug_stream, lexer)
+        print_debug_info(debug, streams.debug_stream, lexer, parser)
 
-def print_debug_info(debug, debug_stream, lexer):
+def print_debug_info(debug, debug_stream, lexer, parser):
     if not debug:
         return
 
     debug_stream.write(f"Tokens: {None if lexer is None else lexer.tokens}\n")
+    debug_stream.write(f"AST: {None if parser is None else parser.ast}\n")
