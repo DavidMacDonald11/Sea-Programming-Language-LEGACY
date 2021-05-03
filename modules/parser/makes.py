991d8f2b0037f23b20ea89335bed5ee48ae5bb06
David MacDonald <davidjmacdonald11@gmail.com>
2021-05-03 11:59:43 -0400

    Add comparison expression parsing

--- modules/parser/makes.py
+++ modules/parser/makes.py
@@ -1,12 +1,14 @@
 from types import SimpleNamespace
-from .grammar.boolean_expression import make_boolean_not_expression
-from .grammar.boolean_expression import make_boolean_and_expression
-from .grammar.boolean_expression import make_boolean_or_expression
+from .grammar.expressions.comparison_expression import make_comparison_expression
+from .grammar.expressions.boolean_expression import make_boolean_not_expression
+from .grammar.expressions.boolean_expression import make_boolean_and_expression
+from .grammar.expressions.boolean_expression import make_boolean_or_expression
 from .grammar.expression import make_expression, binary_operation, ternary_operation
 from .grammar.line import make_line
 from .grammar.block import make_block, make_block_or_expression
 
 MAKES = SimpleNamespace(
+    comparison_expression = make_comparison_expression,
     boolean_not_expression = make_boolean_not_expression,
     boolean_and_expression = make_boolean_and_expression,
     boolean_or_expression = make_boolean_or_expression,
