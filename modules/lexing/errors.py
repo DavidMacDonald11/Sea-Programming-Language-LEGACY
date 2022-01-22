0b55f4a36f83606cc8e14102ebdd0991b37d5e50
David MacDonald <davidjmacdonald11@gmail.com>
2022-01-22 16:55:44 -0500

    Add specialized lexer errors

--- modules/lexing/errors.py
+++ modules/lexing/errors.py
@@ -8,3 +8,23 @@ class LexerError(SeaError):
             position = type(self).lexer.position.copy()
 
         super().__init__(position, message)
+
+class UnknownSymbolError(LexerError):
+    def __init__(self, symbol, message = ""):
+        self.symbol = symbol
+        super().__init__(message = message)
+
+    def get_message(self):
+        return f"Symbol \"{self.symbol}\" is unknown to the lexer."
+
+class UnknownOperatorError(LexerError):
+    def __init__(self, operator, message = ""):
+        self.operator = operator
+        super().__init__(message = message)
+
+    def get_message(self):
+        return f"Operator \"{self.operator}\" is unknown to the lexer."
+
+class FloatError(LexerError):
+    def get_message(self):
+        return "A float cannot contain more than one decimal point."
