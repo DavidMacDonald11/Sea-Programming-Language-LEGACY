322936145ab998a883f4d56219d0182433348666
David MacDonald <davidjmacdonald11@gmail.com>
2022-05-26 16:25:54 -0400

    Finish refactor and fix x-coord crash

--- modules/interfaces/terminal.py
+++ modules/interfaces/terminal.py
@@ -4,7 +4,7 @@ from .streams.terminal import TerminalInStream, TerminalOutStream, TerminalError
 from . import general
 
 def interface(screen, debug):
-    terminal = NewTerminal(screen)
+    terminal = Terminal(screen)
 
     streams = StreamHolder(
         TerminalInStream(),
@@ -124,33 +124,37 @@ class Cursor:
         y = oy if y is None else y
         x = ox if x is None else x
 
-        y += y_delta
-        x += x_delta
+        y_delta += y - oy
+        x_delta += x - ox
 
-        self.scroll = 0
-        self.screen.move(0, 0)
-
-        for _ in range(y):
+        while y_delta > 0:
             self.move_down()
+            y_delta -= 1
 
-        for _ in range(x):
+        while y_delta < 0:
+            self.move_up()
+            y_delta += 1
+
+        while x_delta > 0:
             self.move_right()
+            x_delta -= 1
+
+        while x_delta < 0:
+            self.move_left()
+            x_delta += 1
 
     def adjust(self, len_prompt, line):
-        _, max_x = self.screen.getmaxyx()
         self.inline = len(line)
-
-        line_wrap = self.line_wrap
-        self.line_wrap = (len_prompt + self.inline) // max_x
+        line_wrap, self.line_wrap = self.line_wrap, 0
         self.move(x = len_prompt + self.inline, y_delta = -line_wrap)
 
 # TODO allow window scrolling
 # TODO fix insert issues
 
-class NewTerminal:
+class Terminal:
     @property
     def history(self):
-        return "".join(self.printed) + self.line + str(self.cursor.inline)
+        return "".join(self.printed) + self.line + str(self.cursor.line_wrap)
 
     def __init__(self, screen):
         self.title ="Sea Programming Language"
@@ -314,6 +318,7 @@ class NewTerminal:
         self.line = self.line[:self.cursor.inline] + self.line[self.cursor.inline + 1:]
 
     def enter_key(self):
+        self.cursor.line_wrap = 0
         self.cursor.inline = 0
         self.line_i = -1
 
@@ -330,196 +335,3 @@ class NewTerminal:
 
         move = 1 if self.insert_mode and self.cursor.inline != len(self.line) else 0
         self.line = self.line[:self.cursor.inline - 1] + key + self.line[self.cursor.inline - move:]
-
-
-class Terminal:
-    def __init__(self, screen):
-        self.title = "Sea Programming Language"
-        self.prompt = "\nsea > "
-        self.block_prompt = "\n...   "
-        self.printed = self.title + self.prompt
-        self.lines = [""]
-        self.line = ""
-        self.position = -1
-        self.cursor = 0
-        self.insert_mode = False
-
-        self.screen = screen
-        self.clear()
-
-    def safe_move_vertical(self, amount = 1):
-        y, _ = self.screen.getyx()
-        max_y, max_x = self.screen.getmaxyx()
-
-        if y + amount == max_y:
-            curses.resize_term(y + amount + 1, max_x)
-            self.screen.refresh()
-
-        return y
-
-    def clear(self):
-        self.printed = self.title + self.prompt
-        self.screen.move(1, len(self.prompt) - 1)
-        self.update_screen()
-
-    def write(self, text = ""):
-        amount = text.count("\n")
-
-        y = self.safe_move_vertical(amount)
-        self.screen.move(y + amount, 0)
-
-        self.printed += text
-        self.update_screen()
-
-    def update_screen(self):
-        cursor = self.screen.getyx()
-        self.screen.clear()
-        self.screen.addstr(self.printed + self.line)
-        self.screen.move(*cursor)
-        self.screen.refresh()
-
-    def new_prompt(self, prompt):
-        y = self.safe_move_vertical()
-        self.screen.move(y + 1, len(prompt) - 1)
-        self.printed += prompt
-
-    def input(self):
-        while True:
-            line = self.line
-            key = self.get_key()
-            self.update_screen()
-
-            if key == "\n":
-                yield line
-
-    def get_key(self):
-        key = self.screen.getkey()
-
-        match key:
-            case "KEY_RESIZE":
-                pass
-            case "KEY_UP":
-                self.up()
-            case "KEY_DOWN":
-                self.down()
-            case "KEY_PPAGE":
-                self.page_up()
-            case "KEY_NPAGE":
-                self.page_down()
-            case "KEY_LEFT":
-                self.left()
-            case "KEY_RIGHT":
-                self.right()
-            case "KEY_DC":
-                self.delete()
-            case "KEY_IC":
-                self.insert()
-            case "KEY_HOME":
-                self.home()
-            case "KEY_END":
-                self.end()
-            case ("\x7f"|"KEY_BACKSPACE"):
-                self.backspace()
-            case "\n":
-                self.enter()
-            case _:
-                self.character(key)
-
-        return key
-
-    def up(self):
-        if len(self.lines) == 1 and self.lines[0] == "":
-            self.line, self.lines[0] = self.lines[0], self.line
-
-        if abs(self.position) == len(self.lines):
-            return
-
-        if self.position == -1:
-            self.lines[-1] = self.line
-
-        self.shift_line(-1)
-
-    def down(self):
-        if len(self.lines) == 1 and self.lines[0] != "":
-            self.line, self.lines[0] = self.lines[0], self.line
-
-        if self.position == -1:
-            return
-
-        self.shift_line(1)
-
-    def page_up(self):
-        if self.position == -1:
-            self.lines[-1] = self.line
-
-        self.position = -len(self.lines)
-        self.shift_line(0)
-
-    def page_down(self):
-        self.position = -1
-        self.shift_line(0)
-
-    def shift_line(self, direction):
-        self.position += direction
-        self.line = self.lines[self.position]
-        self.cursor = len(self.line)
-
-        y, _ = self.screen.getyx()
-        self.screen.move(y, len(self.prompt) - 1 + self.cursor)
-
-    def left(self):
-        if self.cursor == 0:
-            return
-
-        self.slide_cursor(-1)
-
-    def right(self):
-        if self.cursor == len(self.line):
-            return
-
-        self.slide_cursor(1)
-
-    def slide_cursor(self, amount):
-        self.cursor += amount
-
-        y, x = self.screen.getyx()
-        self.screen.move(y, x + amount)
-
-    def delete(self):
-        if self.cursor == len(self.line):
-            return
-
-        self.line = self.line[:self.cursor] + self.line[self.cursor + 1:]
-
-    def insert(self):
-        self.insert_mode = not self.insert_mode
-
-    def home(self):
-        self.slide_cursor(-self.cursor)
-
-    def end(self):
-        self.slide_cursor(len(self.line) - self.cursor)
-
-    def backspace(self):
-        if self.cursor == 0:
-            return
-
-        self.slide_cursor(-1)
-        self.line = self.line[:self.cursor] + self.line[self.cursor + 1:]
-
-    def enter(self):
-        self.cursor = 0
-        self.position = -1
-
-        self.printed += self.line
-        self.lines[-1] = self.line
-
-        if self.line != "":
-            self.lines.append("")
-            self.line = ""
-
-    def character(self, key):
-        self.slide_cursor(1)
-
-        shift = 1 if self.insert_mode and self.cursor != len(self.line) else 0
-        self.line = self.line[:self.cursor - 1] + key + self.line[self.cursor - shift:]
